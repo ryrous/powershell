48328a4494881e73eec3b36118c3a7bf6f710fc3
RyRoUK <38826351+RyRoUK@users.noreply.github.com>
2018-08-31 17:18:50 -0700

    Toolbox

--- ADUsersMove.ps1
+++ ADUsersMove.ps1
@@ -1,15 +1,15 @@
-#Export User Accounts to CSV for Reference
-Search-ADAccount -SearchBase "OU=Users,DC=domain,DC=com" -UsersOnly -AccountInactive -TimeSpan 90.00:00:00 | Select-Object -Property SAMaccountname, Enabled, PasswordExpired, PasswordNeverExpires, LastLogonDate | Export-Csv C:\ExportDir\InactiveUsers.csv -NoTypeInformation
-Search-ADAccount -SearchBase "OU=Users,DC=domain,DC=com" -UsersOnly -AccountExpired | Select-Object -Property SAMaccountname, Enabled, PasswordExpired, PasswordNeverExpires, LastLogonDate | Export-Csv C:\ExportDir\ExpiredUsers.csv -NoTypeInformation
-Search-ADAccount -SearchBase "OU=Users,DC=domain,DC=com" -UsersOnly -AccountDisabled | Select-Object -Property SAMaccountname, Enabled, PasswordExpired, PasswordNeverExpires, LastLogonDate | Export-Csv C:\ExportDir\DisabledUsers.csv -NoTypeInformation
 #Create Variables
-$InactiveUsers = (Import-Csv -Path C:\ExportDir\InactiveUsers.csv).SAMaccountname
-$ExpiredUsers = (Import-Csv -Path C:\ExportDir\ExpiredUsers.csv).SAMaccountname
-$DisabledUsers = (Import-Csv -Path C:\ExportDir\DisabledUsers.csv).SAMaccountname
-$InactiveOU = "OU=InactiveUsers,OU=Users,DC=domain,DC=com"
-$ExpiredOU = "OU=ExpiredUsers,OU=Users,DC=domain,DC=com"
-$DisabledOU = "OU=DisabledUsers,OU=Users,DC=domain,DC=com"
+$TargetOU = "OU=NameOfOU,DC=domain,DC=com"
+$InactiveOU = "OU=Inactive Users,OU=NameOfOU,DC=domain,DC=com"
+$ExpiredOU = "OU=Expired Users,OU=NameOfOU,DC=domain,DC=com"
+$DisabledOU = "OU=Disabled Users,OU=NameOfOU,DC=domain,DC=com"
+
+#Export User Accounts to CSV for Reference
+Search-ADAccount -SearchBase $TargetOU -AccountInactive -UsersOnly -TimeSpan 90.00:00:00 | Select-Object -Property SAMaccountname,Enabled,PasswordExpired,PasswordNeverExpires,LastLogonDate | Export-Csv C:\ExportDir\InactiveUsers.csv -NoTypeInformation
+Search-ADAccount -SearchBase $TargetOU -AccountExpired -UsersOnly | Select-Object -Property SAMaccountname,Enabled,PasswordExpired,PasswordNeverExpires,LastLogonDate | Export-Csv C:\ExportDir\ExpiredUsers.csv -NoTypeInformation
+Search-ADAccount -SearchBase $TargetOU -AccountDisabled -UsersOnly | Select-Object -Property SAMaccountname,Enabled,PasswordExpired,PasswordNeverExpires,LastLogonDate | Export-Csv C:\ExportDir\DisabledUsers.csv -NoTypeInformation
+
 #Move User Accounts to new OU in AD
-Move-ADObject -Identity $InactiveUsers -TargetPath $InactiveOU
-Move-ADObject -Identity $ExpiredUsers -TargetPath $ExpiredOU
-Move-ADObject -Identity $DisabledUsers -TargetPath $DisabledOU
+Search-ADAccount -SearchBase $TargetOU -AccountInactive -UsersOnly -TimeSpan 90.00:00:00 | Move-ADObject -TargetPath $InactiveOU
+Search-ADAccount -SearchBase $TargetOU -AccountExpired -UsersOnly | Move-ADObject -TargetPath $ExpiredOU
+Search-ADAccount -SearchBase $TargetOU -AccountDisabled -UsersOnly | Move-ADObject -TargetPath $DisabledOU
--- ADUsersRemove.ps1
+++ ADUsersRemove.ps1
@@ -1,8 +1,12 @@
+#Create Variables
+$TargetOU = "OU=NameOfOU,DC=domain,DC=com"
+
 #Export User Accounts to CSV for Reference
-Search-ADAccount -SearchBase "OU=Users,DC=domain,DC=com" -UsersOnly -AccountInactive -TimeSpan 90.00:00:00 | Select-Object -Property SAMaccountname, Enabled, PasswordExpired, PasswordNeverExpires, LastLogonDate | Export-Csv C:\ExportDir\InactiveUsers.csv
-Search-ADAccount -SearchBase "OU=Users,DC=domain,DC=com" -UsersOnly -AccountExpired | Select-Object -Property SAMaccountname, Enabled, PasswordExpired, PasswordNeverExpires, LastLogonDate | Export-Csv C:\ExportDir\ExpiredUsers.csv
-Search-ADAccount -SearchBase "OU=Users,DC=domain,DC=com" -UsersOnly -AccountDisabled | Select-Object -Property SAMaccountname, Enabled, PasswordExpired, PasswordNeverExpires, LastLogonDate | Export-Csv C:\ExportDir\DisabledUsers.csv
-#Remove User Accounts from AD
-Search-ADAccount -SearchBase "OU=Users,DC=domain,DC=com" -UsersOnly -AccountInactive -TimeSpan 90.00:00:00 | Remove-ADUser
-Search-ADAccount -SearchBase "OU=Users,DC=domain,DC=com" -UsersOnly -AccountExpired | Remove-ADUser
-Search-ADAccount -SearchBase "OU=Users,DC=domain,DC=com" -UsersOnly -AccountDisabled | Remove-ADUser
+Search-ADAccount -SearchBase $TargetOU -AccountInactive -UsersOnly -TimeSpan 90.00:00:00 | Select-Object -Property SAMaccountname,Enabled,PasswordExpired,PasswordNeverExpires,LastLogonDate | Export-Csv C:\ExportDir\InactiveUsers.csv -NoTypeInformation
+Search-ADAccount -SearchBase $TargetOU -AccountExpired -UsersOnly | Select-Object -Property SAMaccountname,Enabled,PasswordExpired,PasswordNeverExpires,LastLogonDate | Export-Csv C:\ExportDir\ExpiredUsers.csv -NoTypeInformation
+Search-ADAccount -SearchBase $TargetOU -AccountDisabled -UsersOnly | Select-Object -Property SAMaccountname,Enabled,PasswordExpired,PasswordNeverExpires,LastLogonDate | Export-Csv C:\ExportDir\DisabledUsers.csv -NoTypeInformation
+
+#Move User Accounts to new OU in AD
+Search-ADAccount -SearchBase $TargetOU -AccountInactive -UsersOnly -TimeSpan 90.00:00:00 | Remove-ADUser
+Search-ADAccount -SearchBase $TargetOU -AccountExpired -UsersOnly | Remove-ADUser
+Search-ADAccount -SearchBase $TargetOU -AccountDisabled -UsersOnly | Remove-ADUser
