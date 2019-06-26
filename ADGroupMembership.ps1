11a80a95725394d0884e1a420a245fdd2258b3c1
Ry Ro <ry@ryroberts.com>
2019-06-26 11:03:22 -1000

    powershell

--- ADGroupMembership.ps1
+++ ADGroupMembership.ps1
@@ -1,2 +1,7 @@
-Get-ADGroupMember -Server domain.com -Identity "Domain Admins" -Recursive | Export-Csv -Path C:\ExportDir\DomainAdmins.csv
-Get-ADGroupMember -Server domain.com -Identity "Local Admin Users" -Recursive | Export-Csv -Path C:\ExportDir\LocalAdmins.csv 
+Get-ADGroupMember -Server domain.com `
+                  -Identity "Domain Admins" `
+                  -Recursive | Export-Csv -Path C:\ExportDir\DomainAdmins.csv
+                  
+Get-ADGroupMember -Server domain.com `
+                  -Identity "Local Admin Users" `
+                  -Recursive | Export-Csv -Path C:\ExportDir\LocalAdmins.csv
--- ADInactiveAccounts.ps1
+++ ADInactiveAccounts.ps1
@@ -1,13 +1,11 @@
 ### SEARCH 90 DAY INACTIVE ACCOUNTS BY USERS ONLY ###
 Search-ADAccount -UsersOnly `
                  -AccountInactive `
-                 -TimeSpan 90.00:00:00 `
-| Export-Csv C:\ExportDir\InactiveAccounts.csv -NoTypeInformation
+                 -TimeSpan 90.00:00:00 | Export-Csv C:\ExportDir\InactiveAccounts.csv -NoTypeInformation
 
 
 ### SEARCH 90 DAY INACTIVE ACCOUNTS BY OU ###
 Search-ADAccount -SearchBase "OU=name,OU=name,DC=domain,DC=com" `
                  -UsersOnly `
                  -AccountInactive `
-                 -TimeSpan 90.00:00:00 `
-| Export-Csv C:\ExportDir\InactiveAccounts.csv -NoTypeInformation
+                 -TimeSpan 90.00:00:00 | Export-Csv C:\ExportDir\InactiveAccounts.csv -NoTypeInformation
