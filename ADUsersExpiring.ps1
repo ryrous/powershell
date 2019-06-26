25a9f28d52e02b82111bf2b4e956572e600e60c5
Ry Ro <ry@ryroberts.com>
2019-06-26 11:06:32 -1000

    powershell

--- ADUsersExpiring.ps1
+++ ADUsersExpiring.ps1
@@ -1,7 +1,7 @@
-#Export User Accounts to CSV for Reference
-Search-ADAccount -UsersOnly -AccountExpiring | Select-Object -Property SAMaccountname, `
+Search-ADAccount -UsersOnly `
+                 -AccountExpiring | Select-Object -Property SAMaccountname, `
                                                                        Enabled, `
                                                                        PasswordExpired, `
                                                                        PasswordNeverExpires, `
                                                                        LastLogonDate `
-                                             | Export-Csv C:\ExportDir\ExpiringUsers.csv -NoTypeInformation
+                                  | Export-Csv C:\ExportDir\ExpiringUsers.csv -NoTypeInformation
