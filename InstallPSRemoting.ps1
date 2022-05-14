c97758285556ecf0830a44cd6181667c1dd15b8c
Your Name <you@example.com>
2022-05-13 14:22:42 -1000

    powershell

--- InstallPSRemoting.ps1
+++ InstallPSRemoting.ps1
@@ -18,25 +18,19 @@
 #
 #####################################################################################################
 [CmdletBinding(DefaultParameterSetName = "NotByPath")]
-param
-(
+param (
     [parameter(Mandatory = $true, ParameterSetName = "ByPath")]
     [switch]$Force,
     [string]
     $PowerShellHome
 )
-
 Set-StrictMode -Version Latest
-
-if (! ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator"))
-{
+if (! ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")){
     Write-Error "WinRM registration requires Administrator rights. To run this cmdlet, start PowerShell with the `"Run as administrator`" option."
     return
 }
-function Register-WinRmPlugin
-{
-    param
-    (
+function Register-WinRmPlugin {
+    param (
         #
         # Expected Example:
         # %windir%\\system32\\PowerShell\\6.0.0\\pwrshplugin.dll
@@ -58,12 +52,11 @@ function Register-WinRmPlugin
     $regKey = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\WSMAN\Plugin\$pluginEndpointName"
 
     $pluginArchitecture = "64"
-    if ($env:PROCESSOR_ARCHITECTURE -match "x86" -or $env:PROCESSOR_ARCHITECTURE -eq "ARM")
-    {
+    if ($env:PROCESSOR_ARCHITECTURE -match "x86" -or $env:PROCESSOR_ARCHITECTURE -eq "ARM") {
         $pluginArchitecture = "32"
     }
-    $regKeyValueFormatString = @"
-<PlugInConfiguration xmlns="http://schemas.microsoft.com/wbem/wsman/1/config/PluginConfiguration" Name="{0}" Filename="{1}"
+    $regKeyValueFormatString = @" 
+    <PlugInConfiguration xmlns="http://schemas.microsoft.com/wbem/wsman/1/config/PluginConfiguration" Name="{0}" Filename="{1}"
     SDKVersion="2" XmlRenderingType="text" Enabled="True" OutputBufferingMode="Block" ProcessIdleTimeoutSec="0" Architecture="{2}"
     UseSharedProcess="false" RunAsUser="" RunAsPassword="" AutoRestart="false">
     <InitializationParameters>
@@ -78,19 +71,16 @@ function Register-WinRmPlugin
     </Resources>
     <Quotas IdleTimeoutms="7200000" MaxConcurrentUsers="5" MaxProcessesPerShell="15" MaxMemoryPerShellMB="1024" MaxShellsPerUser="25"
     MaxConcurrentCommandsPerShell="1000" MaxShells="25" MaxIdleTimeoutms="43200000"/>
-</PlugInConfiguration>
+    </PlugInConfiguration> 
 "@
     $valueString = $regKeyValueFormatString -f $pluginEndpointName, $pluginAbsolutePath, $pluginArchitecture
-
     New-Item $regKey -Force > $null
     New-ItemProperty -Path $regKey -Name ConfigXML -Value $valueString > $null
 }
 
-function New-PluginConfigFile
-{
+function New-PluginConfigFile {
     [CmdletBinding(SupportsShouldProcess, ConfirmImpact="Medium")]
-    param
-    (
+    param (
         [string]
         [parameter(Mandatory=$true)]
         [ValidateNotNullOrEmpty()]
@@ -101,12 +91,10 @@ function New-PluginConfigFile
         [ValidateNotNullOrEmpty()]
         $targetPsHomeDir
     )
-
     # This always overwrites the file with a new version of it if the
     # script is invoked multiple times.
     Set-Content -Path $pluginFile -Value "PSHOMEDIR=$targetPsHomeDir" -ErrorAction Stop
     Add-Content -Path $pluginFile -Value "CORECLRDIR=$targetPsHomeDir" -ErrorAction Stop
-
     Write-Verbose "Created Plugin Config File: $pluginFile" -Verbose
 }
 
@@ -124,13 +112,11 @@ function Install-PluginEndpoint {
     #                    #
     ######################
 
-    if ($PowerShellHome -ne $null)
-    {
+    if ($null -ne $PowerShellHome) {
         $targetPsHome = $PowerShellHome
         $targetPsVersion = & "$targetPsHome\pwsh" -NoProfile -Command '$PSVersionTable.PSVersion.ToString()'
     }
-    else
-    {
+    else {
         ## Get the PSHome and PSVersion using the current powershell instance
         $targetPsHome = $PSHOME
         $targetPsVersion = $PSVersionTable.PSVersion.ToString()
@@ -146,54 +132,39 @@ function Install-PluginEndpoint {
             $targetPsVersion = $targetPsVersion.Substring(0, $dotPos)
         }
     }
-
     Write-Verbose "Using PowerShell Version: $targetPsVersion" -Verbose
-
     $pluginEndpointName = "PowerShell.$targetPsVersion"
-
     $endPoint = Get-PSSessionConfiguration $pluginEndpointName -Force:$Force -ErrorAction silentlycontinue 2>&1
-
     # If endpoint exists and -Force parameter was not used, the endpoint would not be overwritten.
-    if ($endpoint -and !$Force)
-    {
+    if ($endpoint -and !$Force) {
         Write-Error -Category ResourceExists -ErrorId "PSSessionConfigurationExists" -Message "Endpoint $pluginEndpointName already exists."
         return
     }
-
     if (!$PSCmdlet.ShouldProcess($pluginEndpointName)) {
         return
     }
-
-    if ($PSVersionTable.PSVersion -lt "6.0")
-    {
+    if ($PSVersionTable.PSVersion -lt "6.0") {
         # This script is primarily used from Windows PowerShell for Win10 IoT and NanoServer to setup PSCore6 remoting endpoint
         # so it's ok to hardcode to 'C:\Windows' for those systems
         $pluginBasePath = Join-Path "C:\Windows\System32\PowerShell" $targetPsVersion
     }
-    else
-    {
+    else {
         $pluginBasePath = Join-Path ([System.Environment]::GetFolderPath([System.Environment+SpecialFolder]::Windows) + "\System32\PowerShell") $targetPsVersion
     }
 
     $resolvedPluginAbsolutePath = ""
-    if (! (Test-Path $pluginBasePath))
-    {
+    if (! (Test-Path $pluginBasePath)) {
         Write-Verbose "Creating $pluginBasePath"
         $resolvedPluginAbsolutePath = New-Item -Type Directory -Path $pluginBasePath
     }
-    else
-    {
+    else {
         $resolvedPluginAbsolutePath = Resolve-Path $pluginBasePath
     }
-
     $pluginPath = Join-Path $resolvedPluginAbsolutePath "pwrshplugin.dll"
-
     # This is forced to ensure the the file is placed correctly
     Copy-Item $targetPsHome\pwrshplugin.dll $resolvedPluginAbsolutePath -Force -Verbose -ErrorAction Stop
-
     $pluginFile = Join-Path $resolvedPluginAbsolutePath "RemotePowerShellConfig.txt"
     New-PluginConfigFile $pluginFile (Resolve-Path $targetPsHome)
-
     # Register the plugin
     Register-WinRmPlugin $pluginPath $pluginEndpointName
 
@@ -203,29 +174,21 @@ function Install-PluginEndpoint {
     #                                                                  #
     ####################################################################
 
-    if (! (Test-Path $pluginFile))
-    {
+    if (! (Test-Path $pluginFile)) {
         throw "WinRM Plugin configuration file not created. Expected = $pluginFile"
     }
-
-    if (! (Test-Path $resolvedPluginAbsolutePath\pwrshplugin.dll))
-    {
+    if (! (Test-Path $resolvedPluginAbsolutePath\pwrshplugin.dll)) {
         throw "WinRM Plugin DLL missing. Expected = $resolvedPluginAbsolutePath\pwrshplugin.dll"
     }
-
-    try
-    {
+    try {
         Write-Host "`nGet-PSSessionConfiguration $pluginEndpointName" -foregroundcolor "green"
         Get-PSSessionConfiguration $pluginEndpointName -ErrorAction Stop
     }
-    catch [Microsoft.PowerShell.Commands.WriteErrorException]
-    {
+    catch [Microsoft.PowerShell.Commands.WriteErrorException] {
         throw "No remoting session configuration matches the name $pluginEndpointName."
     }
 }
-
 Install-PluginEndpoint -Force $Force
 Install-PluginEndpoint -Force $Force -VersionIndependent
-
 Write-Host "Restarting WinRM to ensure that the plugin configuration change takes effect.`nThis is required for WinRM running on Windows SKUs prior to Windows 10." -foregroundcolor Magenta
 Restart-Service winrm
