d9202a9c61a860646c92b4c42149151335361adb
ryrous <ryrousa@gmail.com>
2022-05-18 13:38:52 -1000

    powershell

--- CopyFile2RemotePC.ps1
+++ CopyFile2RemotePC.ps1
@@ -1,6 +1,5 @@
-$pcList = "pc1","pc2"
-foreach ($pc in $pcList) {
-    $MYSESSION = New-PSSession -ComputerName $pc
-    Copy-Item –Path "C:\test.txt" –Destination "C:\" –ToSession $MYSESSION
-    Remove-PSSession -Session $MYSESSION 
-}
+Get-Credential domain\usermname
+$PC = Read-Host ".\ListOfPCs.csv"
+$FileLocation = Read-Host ".\filename.ext"
+$FileDestination = Read-Host "C:\temp\"
+Get-Content $PC | foreach {Copy-Item $FileLocation -Destination \\$_\c$\$FileDestination}
