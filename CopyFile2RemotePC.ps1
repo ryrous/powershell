0cbeccca08319270a78c48d7d0fc199e37f74dc4
ryrous <ryrousa@gmail.com>
2022-05-18 13:03:26 -1000

    powershell

--- CopyFile2RemotePC.ps1
+++ CopyFile2RemotePC.ps1
@@ -1,6 +1,6 @@
 $pcList = "pc1","pc2"
 foreach ($pc in $pcList) {
-    $MYSESSION = New-PSSession 
+    $MYSESSION = New-PSSession -ComputerName $pc
     Copy-Item –Path "C:\test.txt" –Destination "C:\" –ToSession $MYSESSION
     Remove-PSSession -Session $MYSESSION 
 }
