31ac80932bf50499b16f4bc139ea2d80493d8650
ryrous <ryrousa@gmail.com>
2022-05-19 13:06:49 -1000

    powershell

--- GetWinEventsLogs.ps1
+++
@@ -1,5 +0,0 @@
-<code class="language-powershell">$StartDate = (Get-Date).AddDays(-3)
-$ProviderName = 'Windows Error Reporting'
-$Logs = Get-WinEvent -FilterHashtable @{LogName='Application'; StartTime=$StartDate; ProviderName=$ProviderName;}
-$Logs.Count
-</code>
