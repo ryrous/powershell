941c249f495b951130a3a36a3de8d3517c62588b
Your Name <you@example.com>
2022-05-20 05:49:45 -1000

    powershell

--- SetARPRetryCount.ps1
+++ SetARPRetryCount.ps1
@@ -1,10 +1,10 @@
-workflow Set-ARPRetryCount {
+function Set-ARPRetryCount {
     $Servers = Import-Csv -Path C:\PowerShell\ServerList.csv 
-    parallel {
-        $Servers
+    foreach ($Server in $Servers) {
+        InlineScript {
+            New-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Services\Tcpip\Parameters" -Name "ArpRetryCount" -Value 0 -Force
+        }  
     }
-    InlineScript {
-        New-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Services\Tcpip\Parameters" -Name "ArpRetryCount" -Value 0 -Force
-    }
+   
 }
 Set-ARPRetryCount -AsJob -JobName "ArpRetryCount"
