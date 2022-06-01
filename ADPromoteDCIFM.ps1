7041331aac7c37cd97a091de69aeea675edd6605
Ryan Roberts <rroberts@ssiusa.com>
2022-05-31 20:29:54 -1000

    powershell

---
+++ ADPromoteDCIFM.ps1
@@ -0,0 +1,14 @@
+# Install Server Role
+Install-WindowsFeature AD-Domain-Services -IncludeManagementTools
+# Install PowerShell Module
+Import-Module ADDSDeployment
+# Promote to DC
+Install-ADDSDomainController -DomainName lucernpub.com `
+                            -Credential (Get-Credential) `
+                            -installDNS:$true `
+                            -NoGlobalCatalog:$false `
+                            -DatabasePath "E:\NTDS" `
+                            -Logpath "E:\Logs" `
+                            -SysvolPath "E:” `
+                            -Sitename RemoteLocation `
+                            -InstallationMediaPath "C:\IFM"
