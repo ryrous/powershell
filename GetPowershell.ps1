7235d7a82830b585817d408f9824a68f02c552ef
Ry Ro <ryroberts1@outlook.com>
2022-06-22 07:09:55 -1000

    powershell

--- GetPowershell.ps1
+++ GetPowershell.ps1
@@ -1,3 +1,3 @@
 Invoke-WebRequest -Uri https://github.com/PowerShell/PowerShell/releases/download/v7.2.4/PowerShell-7.2.4-win-x64.msi -Outfile C:\Temp\PowerShell-7.2.4.msi
-cd c:\temp 
+Set-Location C:\tTemp 
 .\PowerShell-7.2.4.msi 
