fb99d3c26402b49ca5b0bdd351b4dba875004d28
Ry Ro <ryroberts1@outlook.com>
2022-06-21 22:00:29 -1000

    powershell

--- GetServiceStatus.ps1
+++ GetServiceStatus.ps1
@@ -1,14 +1,8 @@
-if (!$args.count -ne 1) { 
-    $rh = Read-Host "Enter the name of the service to check" 
-    $myValue = Get-Service $rh 
-} 
-else { 
-    $myValue = Get-Service $args[0] 
-} 
-$serName = $myValue.Name 
-switch -wildcard ($myValue.Status) { 
-    "S*" {Write-Host "The $serName service is stopped."} 
-    "R*" {Write-Host "The $serName service is running."} 
-    "P*" {Write-Host "The $serName service is paused."} 
+$svc = Get-Service W32Time 
+$svcName = $svc.Name 
+switch -wildcard ($svc.Status) { 
+    "S*" {Write-Host "The $svcName service is stopped."} 
+    "R*" {Write-Host "The $svcName service is running."} 
+    "P*" {Write-Host "The $svcName service is paused."} 
     default {Write-Host "Check the service."} 
 }
