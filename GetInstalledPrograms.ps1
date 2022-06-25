7ef05fc7eb2a3a8bacce1e86318b4c8a269b920f
Ry Ro <ryroberts1@outlook.com>
2022-06-25 13:10:59 -1000

    powershell

--- GetInstalledPrograms.ps1
+++ GetInstalledPrograms.ps1
@@ -1,21 +1,21 @@
-$computers = Import-Csv "C:\PowerShell\ServerList.csv"
+$Servers = Import-Csv .\ServerList.csv
 $array = @()
-foreach($pc in $computers){
-    $computername=$pc.computername
+foreach($Server in $Servers){
+    $ServerName=$Server.ComputerName
     #Define the variable to hold the location of Currently Installed Programs
-    $UninstallKey="SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Uninstall" 
+    $UninstallKeyLocation="SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Uninstall" 
     #Create an instance of the Registry Object and open the HKLM base key
-    $reg=[microsoft.win32.registrykey]::OpenRemoteBaseKey('LocalMachine',$computername) 
+    $Reg=[microsoft.win32.registrykey]::OpenRemoteBaseKey('LocalMachine',$ServerName) 
     #Drill down into the Uninstall key using the OpenSubKey Method
-    $regkey=$reg.OpenSubKey($UninstallKey) 
+    $RegKey=$Reg.OpenSubKey($UninstallKeyLocation) 
     #Retrieve an array of string that contain all the subkey names
-    $subkeys=$regkey.GetSubKeyNames() 
+    $subkeys=$RegKey.GetSubKeyNames() 
     #Open each Subkey and use GetValue Method to return the required values for each
     foreach($key in $subkeys){
-        $thisKey=$UninstallKey+"\\"+$key 
-        $thisSubKey=$reg.OpenSubKey($thisKey) 
+        $thisKey=$UninstallKeyLocation+"\\"+$key 
+        $thisSubKey=$Reg.OpenSubKey($thisKey) 
         $obj = New-Object PSObject
-        $obj | Add-Member -MemberType NoteProperty -Name "ComputerName" -Value $computername
+        $obj | Add-Member -MemberType NoteProperty -Name "ComputerName" -Value $ServerName
         $obj | Add-Member -MemberType NoteProperty -Name "DisplayName" -Value $($thisSubKey.GetValue("DisplayName"))
         $obj | Add-Member -MemberType NoteProperty -Name "DisplayVersion" -Value $($thisSubKey.GetValue("DisplayVersion"))
         $obj | Add-Member -MemberType NoteProperty -Name "InstallLocation" -Value $($thisSubKey.GetValue("InstallLocation"))
@@ -23,5 +23,5 @@ foreach($pc in $computers){
         $array += $obj
     } 
 }
-$array | Where-Object { $_.DisplayName } | Select-Object ComputerName, DisplayName, DisplayVersion, Publisher | Format-Table -auto
+$array | Where-Object { $_.DisplayName } | Select-Object ComputerName, DisplayName, DisplayVersion, Publisher | Format-Table -AutoSize
 # Where-Object { $_.DisplayName -and $_.computerName -eq “thisComputer”}
