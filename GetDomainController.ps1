d951af770282fc4d03a78a95f4e66c3be28524f8
Ry Ro <ryroberts1@outlook.com>
2022-06-28 09:18:31 -1000

    powershell

--- GetDomainController.ps1
+++ GetDomainController.ps1
@@ -1,4 +1,2 @@
-$DomainList = Get-Content -Path C:\GetDomainController\DomainList.txt
-foreach ($Domain in $DomainList) {
-    Get-ADDomainController -Discover -DomainName $Domain -Service "PrimaryDC" -ForceDiscover 
-}
+# Get Primary Domain Controller
+Get-ADDomainController -Discover -DomainName $Domain -Service "PrimaryDC" -ForceDiscover 
