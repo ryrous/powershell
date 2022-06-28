8d6c21c2a9a001bac3ba34aeca56afd567b71ec9
Ry Ro <ryroberts1@outlook.com>
2022-06-27 21:11:02 -1000

    powershell

--- GetInstalledPrograms.ps1
+++ GetInstalledPrograms.ps1
@@ -1,27 +1,2 @@
-$Servers = Import-Csv .\ServerList.csv
-$array = @()
-foreach($Server in $Servers){
-    $ServerName=$Server.ComputerName
-    #Define the variable to hold the location of Currently Installed Programs
-    $UninstallKeyLocation="HKLM:Software\Microsoft\Windows\CurrentVersion\Uninstall" 
-    #Create an instance of the Registry Object and open the HKLM base key
-    $Reg=[Microsoft.Win32.RegistryKey]::OpenRemoteBaseKey('LocalMachine',$ServerName) 
-    #Drill down into the Uninstall key using the OpenSubKey Method
-    $RegKey=$Reg.OpenSubKey($UninstallKeyLocation) 
-    #Retrieve an array of string that contain all the subkey names
-    $Keys=$RegKey.GetSubKeyNames() 
-    #Open each Subkey and use GetValue Method to return the required values for each
-    foreach($Key in $Keys){
-        $SelectedKey=$UninstallKeyLocation+"\\"+$Key 
-        $SelectedSubKey=$Reg.OpenSubKey($SelectedKey) 
-        $obj = New-Object PSObject
-        $obj | Add-Member -MemberType NoteProperty -Name "ComputerName" -Value $ServerName
-        $obj | Add-Member -MemberType NoteProperty -Name "DisplayName" -Value $($SelectedSubKey.GetValue("DisplayName"))
-        $obj | Add-Member -MemberType NoteProperty -Name "DisplayVersion" -Value $($SelectedSubKey.GetValue("DisplayVersion"))
-        $obj | Add-Member -MemberType NoteProperty -Name "InstallLocation" -Value $($SelectedSubKey.GetValue("InstallLocation"))
-        $obj | Add-Member -MemberType NoteProperty -Name "Publisher" -Value $($SelectedSubKey.GetValue("Publisher"))
-        $array += $obj
-    } 
-}
-$array | Where-Object {$_.DisplayName} | Select-Object ComputerName, DisplayName, DisplayVersion, Publisher | Format-Table -AutoSize
-# Where-Object {$_.DisplayName -and $_.computerName -eq “thisComputer”}
+# Get Installed Software
+Get-WmiObject -Class Win32_Product | Select-Object -Property Vendor,Name,Version,InstallDate | Sort-Object Vendor | Format-Table -AutoSize
