27c5ee3e3294ca341fad19389777cbe5ec717a76
Ry Ro <ryroberts1@outlook.com>
2022-06-27 21:26:37 -1000

    powershell

--- HardwareAuditLOCAL.ps1
+++ HardwareAuditLOCAL.ps1
@@ -46,6 +46,6 @@ Get-CimInstance -Class Win32_SystemEnclosure -ComputerName $name  | Select-Objec
 Invoke-Expression "$filepath\$name.html"
  
 # Copy to Local C:\HardwareAudits #
-New-Item -ItemType Directory -path "C:\HardwareAudits" -ErrorVariable capturedErrors -ErrorAction SilentlyContinue
-$capturedErrors | foreach-object { if ($_ -notmatch "already exists") { write-error $_ } }
-Copy-Item "$filepath\$name.html" -Destination 'C:\HardwareAudits'
+New-Item -ItemType Directory -path "C:\Temp\HardwareAudits" -ErrorVariable CapturedErrors -ErrorAction SilentlyContinue
+$capturedErrors | Foreach-Object {if ($_ -notmatch "already exists") {Write-Error $_ }}
+Copy-Item "$filepath\$name.html" -Destination 'C:\Temp\HardwareAudits'
