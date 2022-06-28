d96a961cb088d9d5a192177c2aeb6bce0e8815e6
Ry Ro <ryroberts1@outlook.com>
2022-06-28 07:39:58 -1000

    powershell

--- GetMoboInfo.ps1
+++ GetMoboInfo.ps1
@@ -1,2 +1,2 @@
-Get-WmiObject Win32_BaseBoard | Export-Csv -Path C:\ExportDir\MBinfo.csv -Force 
-# Format-Table PSComputerName, Manufacturer, Model, Name, Serialnumber, SKU, Product -autosize
+# Get Motherboard Information
+Get-WmiObject Win32_BaseBoard | Select-Object PSComputerName, Manufacturer, Model, Name, Serialnumber, SKU, Product | Sort-Object PSComputerName | Format-Table -Autosize
