cde05b1b4342633715e9b4d834959dfbd001044e
Ry Ro <ryroberts1@outlook.com>
2022-06-28 07:30:59 -1000

    powershell

--- HardwareAudit.ps1
+++ HardwareAudit.ps1
@@ -3,35 +3,33 @@ $Servers = Get-Content C:\Temp\Serverlist.txt
 #Run the commands for each server in the list
 $Report = @()
 Foreach ($Server in $Servers) {
-    $CPUInfo = Get-WmiObject Win32_Processor -ComputerName $Server #Get CPU Information
-    $OSInfo = Get-WmiObject Win32_OperatingSystem -ComputerName $Server #Get OS Information
+    $CPU = Get-WmiObject Win32_Processor #Get CPU Information
+    $OSInfo = Get-WmiObject Win32_OperatingSystem #Get OS Information
     #Get Memory Information. The data will be shown in a table as MB, rounded to the nearest MB.
     $OSTotalVirtualMemory = [math]::round($OSInfo.TotalVirtualMemorySize / 1MB)
     $OSTotalVisibleMemory = [math]::round($OSInfo.TotalVisibleMemorySize / 1MB)
     #Get Physical Memory Information. The data will be shown in a table as GB, rounded to the nearest GB.
-    $PhysicalMemory = Get-WmiObject CIM_PhysicalMemory -ComputerName $Server | Measure-Object -Property Capacity -Sum | ForEach-Object {[Math]::Round($_.sum / 1GB)}
+    $PhysicalMemory = Get-WmiObject CIM_PhysicalMemory | Measure-Object -Property Capacity -Sum | ForEach-Object {[Math]::Round($_.sum / 1GB)}
     #Get Disk Information. The data will be shown in a table as GB, rounded to the nearest GB.
-    $DiskInfo = Get-WmiObject -Class Win32_LogicalDisk -ComputerName $Server | Measure-Object -Sum Size | ForEach-Object {[Math]::Round($_.sum / 1GB)}
-    Foreach ($CPU in $CPUInfo) {
-        $ReportObject = New-Object PSObject
-        #The following add data to the infoObjects.	
-        Add-Member -inputObject $ReportObject -memberType NoteProperty -Name "ServerName" -Value $CPU.SystemName
-        Add-Member -inputObject $ReportObject -memberType NoteProperty -Name "Processor" -Value $CPU.Name
-        Add-Member -inputObject $ReportObject -memberType NoteProperty -Name "Model" -Value $CPU.Description
-        Add-Member -inputObject $ReportObject -memberType NoteProperty -Name "Manufacturer" -Value $CPU.Manufacturer
-        Add-Member -inputObject $ReportObject -memberType NoteProperty -Name "Physical Cores" -Value $CPU.NumberOfCores
-        Add-Member -inputObject $ReportObject -memberType NoteProperty -Name "CPU L2 Cache Size" -Value $CPU.L2CacheSize
-        Add-Member -inputObject $ReportObject -memberType NoteProperty -Name "CPU L3 Cache Size" -Value $CPU.L3CacheSize
-        Add-Member -inputObject $ReportObject -memberType NoteProperty -Name "Sockets" -Value $CPU.SocketDesignation
-        Add-Member -inputObject $ReportObject -memberType NoteProperty -Name "Logical Cores" -Value $CPU.NumberOfLogicalProcessors
-        Add-Member -inputObject $ReportObject -memberType NoteProperty -Name "OS Name" -Value $OSInfo.Caption
-        Add-Member -inputObject $ReportObject -memberType NoteProperty -Name "OS Version" -Value $OSInfo.Version
-        Add-Member -inputObject $ReportObject -memberType NoteProperty -Name "Total Physical Memory in GB" -Value $PhysicalMemory
-        Add-Member -inputObject $ReportObject -memberType NoteProperty -Name "TotalVirtual Memory in MB" -Value $OSTotalVirtualMemory
-        Add-Member -inputObject $ReportObject -memberType NoteProperty -Name "TotalVisable Memory in MB" -Value $OSTotalVisibleMemory
-        Add-Member -inputObject $ReportObject -memberType NoteProperty -Name "Total Disk Size" -Value $DiskInfo
-        $ReportObject #Output to the screen for a visual feedback.
-        $Report += $ReportObject
-    }
+    $DiskInfo = Get-WmiObject -Class Win32_LogicalDisk | Measure-Object -Sum Size | ForEach-Object {[Math]::Round($_.sum / 1GB)}
+    $ReportObject = New-Object PSObject
+    #The following add data to the infoObjects.	
+    Add-Member -inputObject $ReportObject -memberType NoteProperty -Name "ServerName" -Value $CPU.SystemName
+    Add-Member -inputObject $ReportObject -memberType NoteProperty -Name "Processor" -Value $CPU.Name
+    Add-Member -inputObject $ReportObject -memberType NoteProperty -Name "Model" -Value $CPU.Description
+    Add-Member -inputObject $ReportObject -memberType NoteProperty -Name "Manufacturer" -Value $CPU.Manufacturer
+    Add-Member -inputObject $ReportObject -memberType NoteProperty -Name "Physical Cores" -Value $CPU.NumberOfCores
+    Add-Member -inputObject $ReportObject -memberType NoteProperty -Name "CPU L2 Cache Size" -Value $CPU.L2CacheSize
+    Add-Member -inputObject $ReportObject -memberType NoteProperty -Name "CPU L3 Cache Size" -Value $CPU.L3CacheSize
+    Add-Member -inputObject $ReportObject -memberType NoteProperty -Name "Sockets" -Value $CPU.SocketDesignation
+    Add-Member -inputObject $ReportObject -memberType NoteProperty -Name "Logical Cores" -Value $CPU.NumberOfLogicalProcessors
+    Add-Member -inputObject $ReportObject -memberType NoteProperty -Name "OS Name" -Value $OSInfo.Caption
+    Add-Member -inputObject $ReportObject -memberType NoteProperty -Name "OS Version" -Value $OSInfo.Version
+    Add-Member -inputObject $ReportObject -memberType NoteProperty -Name "Total Physical Memory in GB" -Value $PhysicalMemory
+    Add-Member -inputObject $ReportObject -memberType NoteProperty -Name "TotalVirtual Memory in MB" -Value $OSTotalVirtualMemory
+    Add-Member -inputObject $ReportObject -memberType NoteProperty -Name "TotalVisable Memory in MB" -Value $OSTotalVisibleMemory
+    Add-Member -inputObject $ReportObject -memberType NoteProperty -Name "Total Disk Size" -Value $DiskInfo
+    $ReportObject #Output to the screen for a visual feedback.
+    $Report += $ReportObject
 }
 $Report | Export-Csv -Path C:\Temp\Server_Inventory_$((Get-Date).ToString('MM-dd-yyyy')).csv -NoTypeInformation -Force
