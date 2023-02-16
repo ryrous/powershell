2b047de05aab2ad5129d1b0daad547698ea6f09a
ryrous <ryroberts1@outlook.com>
2023-02-16 13:27:26 -1000

    powershell

--- InstallModules.ps1
+++
@@ -1,9 +0,0 @@
-# PowerShell Modules
-Install-Module -Name AWSPowerShell
-Install-Module -Name Azure
-Install-Module -Name ComputerManagementDsc -RequiredVersion 8.5.0 -Force
-Install-Module -Name NetworkingDsc
-Install-Module -Name PackageManagement
-Install-Module -Name PowerShellGet -RequiredVersion 2.2.5 -Force
-Install-Module -Name PSWindowsUpdate
-Install-Module -Name VMware.PowerCLI -AllowClobber
