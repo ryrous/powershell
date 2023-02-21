0c567749711626834a1d7f042291909a327c077d
ryrous <ryroberts1@outlook.com>
2023-02-21 09:16:54 -1000

    powershell

--- ADResetUserPassword.ps1
+++ ADResetUserPassword.ps1
@@ -1,5 +1,9 @@
+# Convert your Administrator password to a secure string
 $Password = ConvertTo-SecureString "password" -AsPlainText -Force
+# Create a new PSCredential object using the domain\username and password
 $Credential = New-Object System.Management.Automation.PSCredential ("Domain\UserName", $Password)
+# Set the domain and username
 $Domain = "domain.com"
 $UserName = "UserName"
+# Reset the password
 Set-ADAccountPassword -Credential $Credential -Server $Domain -Identity $UserName -NewPassword (ConvertTo-SecureString -AsPlainText "NewPassword" -Force)
