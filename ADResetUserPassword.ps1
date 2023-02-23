a07447aeabf2ada9c92d257504ca05043f575a7c
ryrous <ryroberts1@outlook.com>
2023-02-23 11:41:56 -1000

    powershell

--- ADResetUserPassword.ps1
+++ ADResetUserPassword.ps1
@@ -3,7 +3,8 @@ $Password = ConvertTo-SecureString "password" -AsPlainText -Force
 # Create a new PSCredential object using your Administrator domain\username and password
 $Credential = New-Object System.Management.Automation.PSCredential ("Domain\UserName", $Password)
 # Set the domain and username
-$Domain = "domain.com"
-$UserName = "UserName"
+$Domain = Read-Host -Prompt 'Input the users Domain like domain.com'
+$UserName = Read-Host -Prompt 'Input UserName of account you wish to reset'
+$NewPassword = Read-Host -Prompt 'Input new temporary Password for account'
 # Reset the password
-Set-ADAccountPassword -Credential $Credential -Server $Domain -Identity $UserName -NewPassword (ConvertTo-SecureString -AsPlainText "NewPassword" -Force)
+Set-ADAccountPassword -Credential $Credential -Server $Domain -Identity $UserName -NewPassword (ConvertTo-SecureString -AsPlainText $NewPassword -Force)
