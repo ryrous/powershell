beb474d7e01d039d8b9c56dff890cf411cc77619
ryrous <ryroberts1@outlook.com>
2023-03-02 14:15:33 -1000

    powershell

--- Compare2CsvFiles.ps1
+++ Compare2CsvFiles.ps1
@@ -1,15 +1,14 @@
-$Path = "C:\Scripts"
-$CsvFile1 = Import-Csv -Path "$($path)\CsvFile1.csv"
-$CsvFile2 = Import-Csv -Path "$($path)\CsvFile2.csv"
-$UserOutput = @()
-    ForEach ($Name in $CsvFile1) {
-        $NameMatch = $CsvFile2 | Where-Object {$_.Name -eq $Name}
-        If($NameMatch) {
-            # Process the data
-            $UserOutput += New-Object PsObject -Property @{Name =$Name;column1=$NameMatch.column1;column2=$NameMatch.column2}
-        }
-        else {
-            $UserOutput += New-Object PsObject -Property @{Name =$Name;column1 ="NA";column2 ="NA"}
-        }
+#Importing CSV
+$File1 = Import-Csv -Path "C:\Scripts\NewList.csv"
+#Importing CSV 
+$File2 = Import-Csv -Path "C:\Scripts\DecomList.csv"
+ 
+#Compare both CSV files - column SamAccountName
+$Results = Compare-Object -ReferenceObject $File1 -DifferenceObject $File2 -Property Name -IncludeEqual
+ 
+Foreach ($R in $Results) {
+    $Status = $File2 | Where-Object Name -like $R.Name | Select-Object -ExpandProperty Status
+    If ($R.sideindicator -eq "==") {
+        Write-Output "Name: $($R.Name) Status: $($Status)" | Sort-Object -Property Name
     }
-$UserOutput | Format-Table
+}
