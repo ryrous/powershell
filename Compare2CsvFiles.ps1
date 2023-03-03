a9842d183bbac2e0d6307f76c5a2dae8583530fc
ryrous <ryroberts1@outlook.com>
2023-03-03 08:20:12 -1000

    powershell

--- Compare2CsvFiles.ps1
+++ Compare2CsvFiles.ps1
@@ -1,14 +1,13 @@
 #Importing CSV
-$File1 = Import-Csv -Path "C:\Scripts\NewList.csv"
+$File1 = Import-Csv -Path "C:\CsvCompare\NewList.csv"
 #Importing CSV 
-$File2 = Import-Csv -Path "C:\Scripts\DecomList.csv"
+$File2 = Import-Csv -Path "C:\CsvCompare\DecomList.csv"
  
 #Compare both CSV files - column SamAccountName
 $Results = Compare-Object -ReferenceObject $File1 -DifferenceObject $File2 -Property Name -IncludeEqual
- 
-Foreach ($R in $Results) {
-    If ($R.sideindicator -eq "==") {
-        $Status = $File2 | Where-Object Name -like $R.Name | Select-Object -ExpandProperty Status
-        Write-Output "Name: $($R.Name) Status: $($Status)" | Sort-Object -Property Name
+
+ForEach ($Result in $Results) {
+    If ($Result.SideIndicator -eq "==") {
+        Write-Output "Name: $($Result.Name) Status: $($File2.Where({$PSItem.Name -eq $Result.Name}).Status)"
     }
 }
