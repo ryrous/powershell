13ab0fc37a3890d368715b224188c2ae9d9a3ded
ryrous <ryroberts1@outlook.com>
2023-03-29 09:48:41 -1000

    powershell

---
+++ ClearEventLogs.ps1
@@ -0,0 +1,5 @@
+function clear-all-event-logs ($computerName="localhost") {
+   $logs = Get-EventLog -ComputerName $computername -List | ForEach-Object {$_.Log}
+   $logs | ForEach-Object {Clear-EventLog -ComputerName $computername -LogName $_ }
+   Get-EventLog -ComputerName $computername -list
+}
