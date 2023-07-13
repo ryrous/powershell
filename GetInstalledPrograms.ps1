b73a1c24736162e6c86dac7efb765e36fbe6477d
ryrous <ryrous@outlook.com>
2023-07-13 01:04:46 -1000

    powershell

--- GetInstalledPrograms.ps1
+++ GetInstalledPrograms.ps1
@@ -1,2 +1,5 @@
 # Get Installed Software
 Get-WmiObject -Class Win32_Product | Select-Object -Property Vendor,Name,Version,InstallDate | Sort-Object Vendor | Format-Table -AutoSize
+
+# If PowerShell is installed from AppStore
+Get-AppxPackage | Where-Object Name -like "Microsoft.PowerShell" | Select-Object Name, Version
