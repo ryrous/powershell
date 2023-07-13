991b11f661780bc123e9025cf342d64193314b6c
ryrous <ryrous@outlook.com>
2023-07-13 01:52:52 -1000

    powershell

--- GetComputerInfo.ps1
+++ GetComputerInfo.ps1
@@ -1,6 +1,6 @@
 function Get-PCinfo {
-    Write-Host "Getting PowerShell Version.." -ForegroundColor Magenta
-    Get-Host | Select-Object Version, DebuggerEnabled, IsRunspacePushed | Format-Table -AutoSize
+    Write-Host "Getting OS Info.." -ForegroundColor Magenta
+    Get-ComputerInfo | Select-Object OSName, OsVersion | Format-Table -AutoSize
 
     Write-Host "Getting LoggedOn User.." -ForegroundColor Magenta
     Get-WMIObject Win32_LoggedOnUser | Select-Object __Server, Antecedent, Dependent | Format-Table -AutoSize
