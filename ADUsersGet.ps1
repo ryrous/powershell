5e8117c8557d18ac8740870ff2feeb639b33daa8
ryrous <ryrous@outlook.com>
2023-07-25 13:01:07 -1000

    powershell

--- ADUsersGet.ps1
+++ ADUsersGet.ps1
@@ -1,2 +1,4 @@
-### Get all AD Users ###
-Get-ADuser -Filter * -Properties SamAccountName,DisplayName,UserPrincipalName,whenCreated,PasswordLastSet,PasswordNeverExpires,MemberOf,LastLogonDate | Export-csv .\AllUsers.csv -NoTypeInformation -UseCulture
+### GET ALL USER ACCOUNTS ###
+Get-ADuser -Filter * -Properties SamAccountName,DisplayName,UserPrincipalName,whenCreated,PasswordLastSet,PasswordNeverExpires,MemberOf,LastLogonDate `
+			| Sort-Object SamAccountName,DisplayName,UserPrincipalName,whenCreated,PasswordLastSet,PasswordNeverExpires,MemberOf,LastLogonDate `
+			| Export-csv .\AllUsers.csv -NoTypeInformation -UseCulture
