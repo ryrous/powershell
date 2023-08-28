9eaa31d1a7599544ae6e750c7b1f3a3c31af0efc
ryrous <ryrous@outlook.com>
2023-08-28 06:33:35 -1000

    powershell

--- AddCAARecord2AzureDNS.ps1
+++
@@ -1,9 +0,0 @@
-$zonename = "example1.com"
-$resourcegroup = "resourcegroup"
-$certificateauthority = "digicert.com" # Possible values are: `letsencrypt.org`, `digicert.com`
-$incidentreport = "you@example1.com" # This will be your personal email id where you want to receive alerts about the Cert incident reports.
-
-$addcaarecord = @()
-$addcaarecord += New-AzDnsRecordConfig -Caaflags 0 -CaaTag "issue" -CaaValue $certificateauthority
-$addcaarecord += New-AzDnsRecordConfig -Caaflags 0 -CaaTag "iodef" -CaaValue "mailto:$incidentreport"
-New-AzDnsRecordSet -Name "@" -RecordType CAA -ZoneName $zoneName -ResourceGroupName $resourcegroup -Ttl 3600 -DnsRecords ($addcaarecord)
