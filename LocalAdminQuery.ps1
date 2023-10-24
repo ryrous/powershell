ce4ac0ee886b88365c4fc2de96f6b3cdf50faf6a
ryrous <38826351+ryrous@users.noreply.github.com>
2023-10-24 06:20:45 -1000

    powershell

--- LocalAdminQuery.ps1
+++ LocalAdminQuery.ps1
@@ -1,7 +1,7 @@
 # Get who I am
 $Me = whoami.exe
 # Get members of administrators group
-$Admins = Get-LocalGroupMember -Name Administrators | Select-Object -ExpandProperty -Name
+$Admins = Get-LocalGroupMember -Name Administrators | Select-Object Name
 # Check to see if this user is an administrator and act accordingly
 if ($Admins -Contains $Me) {
     Write-Host "$Me is a local administrator" -ForegroundColor Green
