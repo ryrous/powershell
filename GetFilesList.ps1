18e37dc92b27552b9f4c2c749fb8e3b251bd8321
ryrous <38826351+ryrous@users.noreply.github.com>
2024-03-19 14:53:01 -0400

    powershell

--- GetFilesList.ps1
+++ GetFilesList.ps1
@@ -1,1 +1,3 @@
-Get-ChildItem -Path C:\Windows\System32 -Recurse -File | Select-Object -Property Name | Sort-Object -Property Name | Export-Csv -Path C:\Users\Username\Downloads\System32Files.csv -UseCulture
+Get-ChildItem -Path C:\Windows\System32 -Recurse -File | `
+    Select-Object -Property Name | Sort-Object -Property Name | `
+    Export-Csv -Path C:\Temp\System32Files.csv -UseCulture
