64f8050c742e33f16d15abf423f31fe25209332c
ryrous <38826351+ryrous@users.noreply.github.com>
2024-03-19 22:51:03 -0400

    powershell

--- SetComputerNameAndIP.ps1
+++ SetComputerNameAndIP.ps1
@@ -1,4 +1,7 @@
-New-NetIPAddress -InterfaceAlias ethernet -IPAddress 192.168.0.10 -PrefixLength 24 -DefaultGateway 192.168.0.1
+Import-Module NetTCPIP
+Import-Module DnsClient
+
+New-NetIPAddress -InterfaceAlias ethernet -IPAddress 192.168.0.10 -PrefixLength 24 -DefaultGateway 192.168.0.1 -AddressFamily IPv4
 Start-Sleep 3
 Set-DNSClientServerAddress -InterfaceAlias "Ethernet" -ServerAddress 9.9.9.9
 Start-Sleep 3
