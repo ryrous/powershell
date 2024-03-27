59139b71f8aaee5d674623b1fcf1e8d192924b68
ryrous <38826351+ryrous@users.noreply.github.com>
2024-03-27 18:23:10 -0400

    powershell

--- GetEthernetMac4Linux.ps1
+++ GetEthernetMac4Linux.ps1
@@ -1,3 +1,3 @@
 # Get Mac Address of Ethernet Interface
-$mac = ifconfig eth0 | grep ether | awk '{print $2}'
+$mac = ifconfig en0 | grep ether | awk '{print $2}'
 Write-Output "The MAC Address of your ethernet adapter is $mac"
