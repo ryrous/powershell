2ea1354f77256c7f78cb550a60897aa8ddd652fd
ryrous <38826351+ryrous@users.noreply.github.com>
2024-03-27 18:40:54 -0400

    powershell

--- GetPowershell.ps1
+++
@@ -1,3 +0,0 @@
-Invoke-WebRequest -Uri https://github.com/PowerShell/PowerShell/releases/download/v7.2.4/PowerShell-7.2.5-win-x64.msi -Outfile C:\Temp\PowerShell-7.2.5.msi
-Set-Location C:\tTemp 
-.\PowerShell-7.2.5.msi
