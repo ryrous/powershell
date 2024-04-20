8e7aa8bffdaeb83847bee7584194aa51b3667969
ryrous <38826351+ryrous@users.noreply.github.com>
2024-04-20 12:09:47 -0400

    powershell

--- PSInstall.ps1
+++ PSInstall.ps1
@@ -1,3 +1,3 @@
-Invoke-WebRequest -Uri https://github.com/PowerShell/PowerShell/releases/download/v7.4.1/PowerShell-7.4.1-win-x64.msi -Outfile C:\Temp\PowerShell-7.4.1.msi
-Set-Location C:\tTemp 
-.\PowerShell-7.4.1.msi
+Invoke-WebRequest -Uri https://github.com/PowerShell/PowerShell/releases/download/v7.4.2/PowerShell-7.4.2-win-x64.msi -Outfile C:\Temp\PowerShell-7.4.2.msi
+Set-Location C:\Temp 
+.\PowerShell-7.4.2.msi
