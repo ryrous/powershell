bf346c7f7725e1fef065b6507c06544cbc310bd3
ryrous <38826351+ryrous@users.noreply.github.com>
2024-04-26 19:01:38 -0400

    powershell

--- PSInstallModules.ps1
+++ PSInstallModules.ps1
@@ -2,29 +2,29 @@ Write-Output "Setting PSGallery as a Trusted repo"
 Set-PSRepository -Name "PSGallery" -InstallationPolicy Trusted
 Write-Output "PSGallery is Trusted"
 Write-Output "Installing Modules.."
-Install-Module -Name 7Zip4Powershell -Scope AllUsers -Confirm:$false
-Install-Module -Name ActiveDirectoryDsc -Scope AllUsers -Confirm:$false
-Install-Module -Name AWSPowerShell -Scope AllUsers -Confirm:$false
-Install-Module -Name AWS.Tools.Common -Scope AllUsers -Confirm:$false
-Install-Module -Name Azure -Scope AllUsers -Confirm:$false
-Install-Module -Name AzureAD -Scope AllUsers -Confirm:$false
-Install-Module -Name CertificateDsc -Scope AllUsers -Confirm:$false
-Install-Module -Name ChocolateyGet -Scope AllUsers -Confirm:$false
-Install-Module -Name ComputerManagementDsc -Scope AllUsers -Confirm:$false
-Install-Module -Name DellBIOSProvider -Scope AllUsers -Confirm:$false
-Install-Module -Name ExchangeOnlineManagement -Scope AllUsers -Confirm:$false
-Install-Module -Name IISAdministration -Scope AllUsers -Confirm:$false
-Install-Module -Name Microsoft.Graph -Scope AllUsers -Confirm:$false
-Install-Module -Name Microsoft.Online.SharePoint.PowerShell -Scope AllUsers -Confirm:$false
-Install-Module -Name MSOnline -Scope AllUsers -Confirm:$false
-Install-Module -Name NetworkingDsc -Scope AllUsers -Confirm:$false
-Install-Module -Name NuGet -Scope AllUsers -Confirm:$false
-Install-Module -Name PackageManagement -Scope AllUsers -Confirm:$false
-Install-Module -Name PnP.PowerShell -Scope AllUsers -Confirm:$false
-Install-Module -Name PowerShellGet -Scope AllUsers -Force
-Install-Module -Name PSScriptAnalyzer -Scope AllUsers -Confirm:$false
-Install-Module -Name SqlServerDsc -Scope AllUsers -Confirm:$false
-Install-Module -Name VMware.PowerCLI -Scope AllUsers -Confirm:$false
-Install-Module -Name xWindowsUpdate -Scope AllUsers -Confirm:$false
+Install-Module -Name 7Zip4Powershell -Confirm:$false
+Install-Module -Name ActiveDirectoryDsc -Confirm:$false
+Install-Module -Name AWSPowerShell -Confirm:$false
+Install-Module -Name AWS.Tools.Common -Confirm:$false
+Install-Module -Name Azure -Confirm:$false
+Install-Module -Name AzureAD -Confirm:$false
+Install-Module -Name CertificateDsc -Confirm:$false
+Install-Module -Name ChocolateyGet -Confirm:$false
+Install-Module -Name ComputerManagementDsc -Confirm:$false
+Install-Module -Name DellBIOSProvider -Confirm:$false
+Install-Module -Name ExchangeOnlineManagement -Confirm:$false
+Install-Module -Name IISAdministration -Confirm:$false
+Install-Module -Name Microsoft.Graph -Confirm:$false
+Install-Module -Name Microsoft.Online.SharePoint.PowerShell -Confirm:$false
+Install-Module -Name MSOnline -Confirm:$false
+Install-Module -Name NetworkingDsc -Confirm:$false
+Install-Module -Name NuGet -Confirm:$false
+Install-Module -Name PackageManagement -Confirm:$false
+Install-Module -Name PnP.PowerShell -Confirm:$false
+Install-Module -Name PowerShellGet -Force
+Install-Module -Name PSScriptAnalyzer -Confirm:$false
+Install-Module -Name SqlServerDsc -Confirm:$false
+Install-Module -Name VMware.PowerCLI -Confirm:$false
+Install-Module -Name xWindowsUpdate -Confirm:$false
 Write-Output "Finished. Displaying installed modules.."
 Get-InstalledModule
