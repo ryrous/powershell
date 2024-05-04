07c7e05ccba132daeaf7e36fc82aca897d8ff589
ryrous <38826351+ryrous@users.noreply.github.com>
2024-05-03 21:45:18 -0400

    powershell

--- CopyFile2RemotePC.ps1
+++ CopyFile2RemotePC.ps1
@@ -2,4 +2,4 @@ Get-Credential domain\usermname
 $PC = Read-Host ".\ListOfPCs.csv"
 $FileLocation = Read-Host ".\filename.ext"
 $FileDestination = Read-Host "C:\temp\"
-Get-Content $PC | foreach {Copy-Item $FileLocation -Destination \\$_\c$\$FileDestination}
+Get-Content $PC | ForEach-Object {Copy-Item $FileLocation -Destination \\$_\c$\$FileDestination}
