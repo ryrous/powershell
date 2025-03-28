63450f6cefcd44bf6969607934a9344b46dc8ee9
ryrous <38826351+ryrous@users.noreply.github.com>
2025-03-28 18:09:30 -0400

    powershell

--- GetUserTime.ps1
+++ GetUserTime.ps1
@@ -1,13 +1,16 @@
 function Get-UserTime {
+    # This function retrieves the current time and returns a greeting based on the time of day.
+    $time = Get-Date -Hour (Get-Date).Hour -Minute (Get-Date).Minute -Second (Get-Date).Second
+    # Get the current hour in 24-hour format
     $dateHour = Get-date -UFormat '%H'
     if ($dateHour -le 12) {
-        "Good Morning"
+        "Good Morning, the time is $time"
     }
     ELSeIF ($dateHour -gt 12 -AND $dateHour -le 18) {
-        "Good Afternoon"
+        "Good Afternoon, the time is $time"
     }
     ELSE {
-        "Good Evening"
+        "Good Evening, the time is $time"
     }
 }
 Get-UserTime
